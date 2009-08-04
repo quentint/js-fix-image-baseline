1bbc366c0095ef74c8308e4a4b9bf6487d6965ba
quentin.t <quentin.t@2200158c-2d16-11de-9cad-a3574e5575ef>
2009-08-04 17:14:00 +0000

    Created DIVs now inherit the IMGs CSS classes and id. Thanks to Darrell Foster Kirsop (http://www.anglocomms.tk/).

--- trunk/fib-jquery.js
+++ trunk/fib-jquery.js
@@ -1,21 +1,23 @@
 /* Quentin T + Erik - http://toki-woki.net - Originally coded for Boks (http://toki-woki.net/p/Boks/) */
 function fixImageBaselines(selector) {
-    $(document).ready(function() {
-        var sbl = $.browser.msie ? document.body.currentStyle.lineHeight : $(document.body).css('line-height');
-        var bl = parseInt(sbl);
-        if (sbl.indexOf('pt') != -1) bl = parseFloat(sbl) * (20 / 14.94); // IE!
-        $(selector).each(function() {
-            var $item = $(this);
-            if ($item.height() % bl != 0) {
-                var $d = $('<div />');
-                $d.css({
-                    'padding-bottom': (bl * (Math.ceil($item.height() / bl)) - $item.height()) + 'px',
-                    'background': 'transparent url(' + $item.attr('src') + ') no-repeat 0 0',
-                    'width': $item.width(),
-                    'height': $item.height()
-                });
-                $item.replaceWith($d);
-            }
-        });
-    });
+	$(document).ready(function() {
+		var sbl = $.browser.msie ? document.body.currentStyle.lineHeight : $(document.body).css('line-height');
+		var bl = parseInt(sbl);
+		if (sbl.indexOf('pt') != -1) bl = parseFloat(sbl) * (20 / 14.94); // IE!
+		$(selector).each(function() {
+			var $item = $(this);
+			if ($item.height() % bl != 0) {
+				var $d = $('<div />');
+				$d.css({
+					'padding-bottom': (bl * (Math.ceil($item.height() / bl)) - $item.height()) + 'px',
+					'background': 'transparent url(' + $item.attr('src') + ') no-repeat 0 0',
+					'width': $item.width(),
+					'height': $item.height()
+				});
+				$d.id=$item.id;
+				$d.className=$item.className;
+				$item.replaceWith($d);
+			}
+		});
+	});
 }
