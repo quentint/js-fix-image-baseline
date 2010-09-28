2f4588bac47c7ce624afa86c15458c576d31079e
quentin.t <quentin.t@2200158c-2d16-11de-9cad-a3574e5575ef>
2010-09-28 09:54:44 +0000

    Enhanced MooTools version

--- trunk/fib-mootools.js
+++ trunk/fib-mootools.js
@@ -1,13 +1,25 @@
 /* Quentin T - http://toki-woki.net - Originally coded for Boks (http://toki-woki.net/p/Boks/) */
+var fibDocumentBaseline;
 function fixImageBaselines(selector) {
 	window.addEvent('load', function() {
 		var sbl=$(document.body).getStyle('line-height');
-		var bl=sbl.toInt();
-		if (sbl.indexOf('pt')!=-1) bl=sbl.toFloat()*(20/14.94);	// IE!
+		if (Browser.Engine.trident) fibDocumentBaseline=16*($(document.body).getStyle('font-size').toInt()/100)*sbl;
+		else fibDocumentBaseline=sbl.toInt();
 		$$(selector).each(function(item) {
 			var d=new Element('div');
-			d.setStyles({'height': bl*(Math.ceil(item.height/bl))+'px', 'line-height':0});
+			d.setStyle('line-height', 0);
 			d.wraps(item);
+			fibSetHolderHeight(d);
 		});
+		doFixImageBaselines(selector);
+		window.addEvent('resize', function() {doFixImageBaselines(selector)});
 	});
+}
+function doFixImageBaselines(selector) {
+	$$(selector).each(function(item) {
+		fibSetHolderHeight(item.getParent());
+	});
+}
+function fibSetHolderHeight(d) {
+	d.setStyle('height', fibDocumentBaseline*(Math.ceil(d.getElement('*').height/fibDocumentBaseline))+'px');
 }
